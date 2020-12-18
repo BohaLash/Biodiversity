15f8666125f895f781c86166918a462e80b24933
Bohdan <dogdanlashko@gmail.com>
2020-12-18 07:19:41 +0200

    🌈

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // Biodiversity
+
+function toggleTheme() {
+    if (document.body.className == 'th1')
+        document.body.className = 'th2'
+    else
+        document.body.className = 'th1'
+}
+
+document.getElementById('theme').onclick = toggleTheme
